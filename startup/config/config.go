bf59ba483de9b07f1ea60700b7d6140f0d1ea7f4
MilosZivic75 <zivic.ra75.2018@uns.ac.rs>
2022-05-08 13:29:35 +0200

    Docker update

--- startup/config/config.go
+++ startup/config/config.go
@@ -1,5 +1,10 @@
 package config
 
+import (
+	"fmt"
+	"os"
+)
+
 type Config struct {
 	Port        string
 	ProfilePort string
@@ -7,9 +12,21 @@ type Config struct {
 }
 
 func NewConfig() *Config {
-	return &Config{
-		Port:        "8003",
-		ProfilePort: "8001",
-		ProfileHost: "localhost",
+	if _, err := os.Stat("/.dockerenv"); err == nil {
+		fmt.Println("docker")
+
+		return &Config{
+			Port:        os.Getenv("AUTHENTICATION_SERVICE_PORT"),
+			ProfilePort: os.Getenv("PROFILE_SERVICE_PORT"),
+			ProfileHost: os.Getenv("PROFILE_SERVICE_HOST"),
+		}
+	} else {
+		fmt.Println("local")
+
+		return &Config{
+			Port:        "8003",
+			ProfilePort: "8001",
+			ProfileHost: "localhost",
+		}
 	}
 }
